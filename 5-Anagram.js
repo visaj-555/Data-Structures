8d262cb56c37db5513e8e05cf46388d8722435ec
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-05-07 17:43:43 +0530

    Solved String Anagram

--- 5-Anagram.js
+++ 5-Anagram.js
@@ -3,3 +3,29 @@
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
 
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    const charCount = {};
+    
+    for (let char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    
+    for (let char of t) {
+        if (!charCount[char]) {
+            return false; 
+        }
+        charCount[char]--;
+    }
+    
+    for (let char in charCount) {
+        if (charCount[char] !== 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
